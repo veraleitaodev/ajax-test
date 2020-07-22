aa0a898cefc6bd8d7fccb642d6265adf6697266c
Vera Leitao <veraleitao.dev@gmail.com>
2020-07-22 21:40:04 +0000

    added function to main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 var xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+        document.getElementById("data").innerHTML = this.responseText;
+    }
+};
