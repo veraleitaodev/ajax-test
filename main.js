0815f58739dad35badcd2341c979d640b2398fed
Vera Leitao <veraleitao.dev@gmail.com>
2020-07-23 05:42:01 +0000

    added timeout function to main.js

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 var xhr = new XMLHttpRequest();
 
+xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+
+xhr.send();
+
 xhr.onreadystatechange = function() {
     if(this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
+        data = JSON.parse(this.responseText);
     }
 };
+
+setTimeout(function() {
+    console.log(data);
+}, 500);
