e1d196a4fe896c2fe0242b85f08bfed6222bc5f8
Vera Leitao <veraleitao.dev@gmail.com>
2020-07-23 10:24:48 +0000

    fix: rectify code on html and js files to reflect source files

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 const baseURL = "https://ci-swapi.herokuapp.com/api/";
 
-function getData(cb) {
+function getData(type, cb) {
   var xhr = new XMLHttpRequest();
-
-  xhr.open("GET", baseURL + type + "/");
-
-  xhr.send();
 
   xhr.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       cb(JSON.parse(this.responseText));
     }
   };
+
+  xhr.open("GET", baseURL + type + "/");
+  xhr.send();
 }
 
 function writeToDocument(type) {
     getData(type, function(data) {
         document.getElementById("data").innerHTML = data;
-    })
+    });
 }
