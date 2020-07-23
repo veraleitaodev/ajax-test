6340a932bbbff62db381b979d5dbcb8d13c657fd
Vera Leitao <veraleitao.dev@gmail.com>
2020-07-23 07:54:33 +0000

    replaced previous function ith callback function

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-var xhr = new XMLHttpRequest();
+function getData(cb) {
+  var xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+  xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
 
-xhr.send();
+  xhr.send();
 
-xhr.onreadystatechange = function() {
-    if(this.readyState == 4 && this.status == 200) {
-        data = JSON.parse(this.responseText);
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      cb(JSON.parse(this.responseText));
     }
-};
+  };
+}
 
-setTimeout(function() {
+function printDataToConsole(data) {
     console.log(data);
-}, 500);
+}
+
+getData(printDataToConsole);
